d5d6d461e52cd414278e33da21138af98877c23f
Vitor Kauê <rodriguesvitorkaue@gmail.com>
2026-07-26 21:33:23 -0300

    feat(db): implementa e conecta com o banco de dados

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -5,3 +5,5 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
 ]);
+
+// apontando erros de sintaxe, bugs potenciais e mantendo um padrão visual na equipe.
